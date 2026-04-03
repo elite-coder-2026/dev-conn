d87b84614f6b28c7c1646f01e237cef51e5ae425
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-03 02:13:35 -0500

    add feedController

---
+++ back-end/src/controllers/feedController.js
@@ -0,0 +1,27 @@
+'use strict'
+const feedService = require('../services/feedService')
+
+function parsePagination(query) {
+  return {
+    limit:  Math.min(parseInt(query.limit,  10) || 20, 100),
+    offset: Math.max(parseInt(query.offset, 10) || 0,  0),
+  }
+}
+
+async function getFeed(req, res, next) {
+  try {
+    const { limit, offset } = parsePagination(req.query)
+    const posts = await feedService.getFeed(req.user.id, limit, offset)
+    res.json({ posts, pagination: { limit, offset, count: posts.length } })
+  } catch (err) { next(err) }
+}
+
+async function getUserPosts(req, res, next) {
+  try {
+    const { limit, offset } = parsePagination(req.query)
+    const posts = await feedService.getUserPosts(req.params.userId, req.user.id, limit, offset)
+    res.json({ posts, pagination: { limit, offset, count: posts.length } })
+  } catch (err) { next(err) }
+}
+
+module.exports = { getFeed, getUserPosts }
