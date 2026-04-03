6f8c8fff13609b1a1102de784c5d7877ec02af79
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-03 07:50:56 -0500

    counts the tine from when it was posted

---
+++ back-end/src/utils/timeAgo.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function timeAgo(date) {
+  const secs = Math.floor((Date.now() - new Date(date)) / 1000)
+  if (secs < 60)    return `${secs}s ago`
+  if (secs < 3600)  return `${Math.floor(secs / 60)}m ago`
+  if (secs < 86400) return `${Math.floor(secs / 3600)}h ago`
+  return `${Math.floor(secs / 86400)}d ago`
+}
+
+module.exports = timeAgo
