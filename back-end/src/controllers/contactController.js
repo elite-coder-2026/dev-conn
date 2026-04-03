f8e63f370f4a6a5760c894430032a460adbb8626
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-03 02:13:30 -0500

    add contactController

---
+++ back-end/src/controllers/contactController.js
@@ -0,0 +1,41 @@
+'use strict'
+const contactService = require('../services/contactService')
+
+async function getFollowing(req, res, next) {
+  try {
+    const userId   = req.params.userId || req.user.id
+    const contacts = await contactService.getFollowing(userId)
+    res.json(contacts)
+  } catch (err) { next(err) }
+}
+
+async function getFollowers(req, res, next) {
+  try {
+    const userId    = req.params.userId || req.user.id
+    const followers = await contactService.getFollowers(userId)
+    res.json(followers)
+  } catch (err) { next(err) }
+}
+
+async function follow(req, res, next) {
+  try {
+    const result = await contactService.follow(req.user.id, req.params.userId)
+    res.status(201).json(result)
+  } catch (err) { next(err) }
+}
+
+async function unfollow(req, res, next) {
+  try {
+    const result = await contactService.unfollow(req.user.id, req.params.userId)
+    res.json(result)
+  } catch (err) { next(err) }
+}
+
+async function checkFollowing(req, res, next) {
+  try {
+    const following = await contactService.isFollowing(req.user.id, req.params.userId)
+    res.json({ following })
+  } catch (err) { next(err) }
+}
+
+module.exports = { getFollowing, getFollowers, follow, unfollow, checkFollowing }
