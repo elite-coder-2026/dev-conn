bcc0aa49940dbca533541641cc7783d95c5be4e0
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:34 -0500

    update notification controller

--- back-end/src/controllers/notificationController.js
+++ back-end/src/controllers/notificationController.js
@@ -3,22 +3,14 @@ const notificationService = require('../services/notificationService')
 
 async function list(req, res, next) {
   try {
-    const { userId } = req.query
-    if (!userId) {
-      const e = new Error('userId is required'); e.status = 400; return next(e)
-    }
-    const notifications = await notificationService.getForUser(userId)
+    const notifications = await notificationService.getForUser(req.user.id)
     res.json(notifications)
   } catch (err) { next(err) }
 }
 
 async function markAllRead(req, res, next) {
   try {
-    const { userId } = req.query
-    if (!userId) {
-      const e = new Error('userId is required'); e.status = 400; return next(e)
-    }
-    await notificationService.markAllRead(userId)
+    await notificationService.markAllRead(req.user.id)
     res.status(204).end()
   } catch (err) { next(err) }
 }
