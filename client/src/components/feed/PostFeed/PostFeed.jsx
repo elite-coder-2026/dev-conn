c76d18179178525c621443bdf3e8695c3ab2e5de
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:35 -0500

    update PostFeed component

--- client/src/components/feed/PostFeed/PostFeed.jsx
+++ client/src/components/feed/PostFeed/PostFeed.jsx
@@ -1,11 +1,11 @@
 import PostCard from '../PostCard/PostCard'
 import './PostFeed.css'
 
-export default function PostFeed({ posts }) {
+export default function PostFeed({ posts, onDelete, currentUser }) {
   return (
     <div className="post-feed">
       {posts.map(post => (
-        <PostCard key={post.id} post={post} />
+        <PostCard key={post.id} post={post} onDelete={onDelete} currentUser={currentUser} />
       ))}
     </div>
   )
