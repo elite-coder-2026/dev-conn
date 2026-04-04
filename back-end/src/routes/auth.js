4b839883e20f9888e9054650a28e793c554b4b50
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:34 -0500

    update auth routes

--- back-end/src/routes/auth.js
+++ back-end/src/routes/auth.js
@@ -7,6 +7,7 @@ const router = express.Router()
 
 router.post('/register',         authController.register)
 router.post('/login',            authController.login)
+router.post('/logout',           authController.logout)
 router.put('/password',          requireAuth, authController.changePassword)
 router.delete('/account',        requireAuth, authController.deleteAccount)
 
