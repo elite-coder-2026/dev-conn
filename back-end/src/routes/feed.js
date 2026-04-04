c8806218eac7e2858293c04cd17a141818aaaae5
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:34 -0500

    update feed routes

--- back-end/src/routes/feed.js
+++ back-end/src/routes/feed.js
@@ -6,6 +6,7 @@ const feedController = require('../controllers/feedController')
 const router = express.Router()
 
 router.get('/',                  requireAuth, feedController.getFeed)
+router.get('/components',        requireAuth, feedController.getComponents)
 router.get('/users/:userId',     requireAuth, feedController.getUserPosts)
 
 module.exports = router
