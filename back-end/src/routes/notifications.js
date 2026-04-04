65dcdbd35ea62cf1d7e5c92a8760091f4c2f8e5e
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:34 -0500

    update notification routes

--- back-end/src/routes/notifications.js
+++ back-end/src/routes/notifications.js
@@ -1,10 +1,11 @@
 'use strict'
 const express                = require('express')
+const requireAuth            = require('../middleware/auth')
 const notificationController = require('../controllers/notificationController')
 
 const router = express.Router()
 
-router.get('/',           notificationController.list)
-router.patch('/read-all', notificationController.markAllRead)
+router.get('/',           requireAuth, notificationController.list)
+router.patch('/read-all', requireAuth, notificationController.markAllRead)
 
 module.exports = router
