4d2543458deed5719e320a10cc7c50be3e9687c0
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-04-04 18:54:34 -0500

    update feed controller

--- back-end/src/controllers/feedController.js
+++ back-end/src/controllers/feedController.js
@@ -10,18 +10,25 @@ function parsePagination(query) {
 
 async function getFeed(req, res, next) {
   try {
-    const { limit, offset } = parsePagination(reconnection_queries.query)
-    const posts = await feedService.getFeed(reconnection_queries.user.id, limit, offset)
+    const { limit, offset } = parsePagination(req.query)
+    const posts = await feedService.getFeed(req.user.id, limit, offset)
     res.json({ posts, pagination: { limit, offset, count: posts.length } })
   } catch (err) { next(err) }
 }
 
 async function getUserPosts(req, res, next) {
   try {
-    const { limit, offset } = parsePagination(reconnection_queries.query)
-    const posts = await feedService.getUserPosts(reconnection_queries.params.userId, reconnection_queries.user.id, limit, offset)
+    const { limit, offset } = parsePagination(req.query)
+    const posts = await feedService.getUserPosts(req.params.userId, req.user.id, limit, offset)
     res.json({ posts, pagination: { limit, offset, count: posts.length } })
   } catch (err) { next(err) }
 }
 
-module.exports = { getFeed, getUserPosts }
+async function getComponents(req, res, next) {
+  try {
+    const components = await feedService.getComponents()
+    res.json(components)
+  } catch (err) { next(err) }
+}
+
+module.exports = { getFeed, getUserPosts, getComponents }
